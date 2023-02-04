5636b9ae24ce1d1696f63dc35587ea18d0a188f1
Manuel Pedro <manuelgomesfp2017@gmail.com>
2023-02-04 18:54:57 +0000

    Commit Inicial - CRUD JS PHP E JSON

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>CRUD JS - PHP e JSON</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/estilo.css">
+</head>
+<body>
+
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</body>
+</html>
