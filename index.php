f5737ed99f1123e1461eaf836378134992bddba4
Manuel Pedro <manuelgomesfp2017@gmail.com>
2023-02-04 22:28:18 +0000

    Criação da Página HTML e CSS

--- index.php
+++ index.php
@@ -8,6 +8,53 @@
 </head>
 <body>
 
+	<section>
+		<h1>Formulário de Cadastramento</h1>
+		<form>
+			<div class="form">				
+				<div class="form-area">
+					<label for="nome">Nome</label>
+					<input type="text" name="nome" id="nome" class="form-control" required>
+				</div>
+				<div class="form-area">
+					<label for="sobrenome">Sobrenome</label>
+					<input type="text" name="sobrenome" id="sobrenome" class="form-control" required>
+				</div>
+			</div>
+			<div class="form-btn">
+				<input type="hidden" name="usuario" id="usuario">
+				<button type="submit" class="btn-enviar">Adicionar</button>
+				<button type="reset" class="btn-limpar">Cancelar</button>
+			</div>
+		</form>
+	</section>
+	<section>
+		<h1>Tabela de Registros</h1>
+		<div class="tabela">
+			<table>
+				<thead>
+					<tr>
+						<th>#</th>
+						<th>Nome</th>
+						<th>Sobrenome</th>
+						<th></th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<th>1</th>
+						<td>Manuel</td>
+						<td>Pedro</td>
+						<td class="tabela-control">
+							<button type="button" class="btn-editar">Editar</button>
+							<button type="button" class="btn-eliminar">Eliminar</button>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</section>
+
 	<script type="text/javascript" src="assets/js/script.js"></script>
 </body>
 </html>
